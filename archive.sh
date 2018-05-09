38797c77a36e72ce50f8ff6f375e000f06417c73
Andrey Utkin <andrey_utkin@gentoo.org>
2018-05-09 15:04:22 +0100

    archive.sh: limit rev-list to just one commit

--- archive.sh
+++ archive.sh
@@ -13,7 +13,7 @@ DIR="${DIR%%/}"
 ARCHIVE_NAME="${DIR//\//_}_extras"
 
 # $2, or, if not set, $DIR's last commit
-COMMIT=${2:-$(git rev-list --all HEAD -- "$DIR")}
+COMMIT=${2:-$(git rev-list -n 1 --all HEAD -- "$DIR")}
 # $COMMIT's commit date
 SOURCE_DATE_EPOCH=$(git show --format="format:%at" "$COMMIT" | head -n1)
 
